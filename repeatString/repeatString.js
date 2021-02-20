41600825b6cc27507acc96084925af80bf892520
Tyler Scott <off.nadir.insight@gmail.com>
2021-02-20 10:53:30 -0600

    update repeatString with offline solution

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+	let outputStr = '';
+	if (num < 0) {
+		return 'ERROR';
+}	
+	for (num; num > 0; num--) {
+		outputStr += str;
+	}	
+	return outputStr;
 }
-
+repeatString('hey', 3);
 module.exports = repeatString
